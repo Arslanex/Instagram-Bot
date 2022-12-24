8a78f728846bbe0dab2be16edd42fed2203d6552
Arslanex <44752389+Arslanex@users.noreply.github.com>
2022-12-24 21:54:21 +0300

    links

---
+++ Bot/links.py
@@ -0,0 +1,13 @@
+driver = "chromedriver.exe"
+website = "https://www.instagram.com/"
+
+# SIGN IN
+box1 = "_ac8f"
+box2 = '/html/body/div[2]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[3]/button[2]'
+
+# GET FOLLOWERS - FOLLOWING
+scrollBar1 = "/html/body/div[2]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[2]"
+scrollBar2 = "/html/body/div[2]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[3]"
+scrollBar3 = ""
+
+textbox = "/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/div[1]/section/div/div[2]/div/div/div[2]/div[2]/div/div[2]/div/div/div[2]/textarea"
